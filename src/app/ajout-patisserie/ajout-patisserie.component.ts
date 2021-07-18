92bdfa81fefc8914047caf6aab50a032a76aa059
skanderbac <skanderbac@users.noreply.github.com>
2021-07-18 14:26:06 +0100

    integration template

---
+++ src/app/ajout-patisserie/ajout-patisserie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-patisserie',
+  templateUrl: './ajout-patisserie.component.html',
+  styleUrls: ['./ajout-patisserie.component.css']
+})
+export class AjoutPatisserieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AjoutPatisserieComponent } from './ajout-patisserie/ajout-patisserie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjoutPatisserieComponent
   ],
   imports: [
     BrowserModule
